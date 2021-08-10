bb74648c1925d7da853356b33dd51a5a5b3d05c8
Mariia Yevlakova <mariayevlakova@gmail.com>
2021-08-10 18:58:15 +0300

    add package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "@/views/EventList.vue";
+import EventDetails from "@/views/EventDetails.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
   },
   {
     path: "/about",
